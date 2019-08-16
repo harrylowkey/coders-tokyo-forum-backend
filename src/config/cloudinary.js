3404499ba403313f7161972f200c1edd0ef1de4c
@quang.dang <quang.dang@midasprotocol.io>
2019-08-16 11:24:48 +0700

    reconfig cloudinary

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,14 +1,9 @@
 const cloudinary = require('cloudinary').v2;
-const {
-  cloudinary_name,
-  cloudinary_api_key,
-  cloudinary_api_secret,
-} = require('./vars');
 
 exports.config = () => {
   cloudinary.config({
-    cloud_name: cloudinary_name,
-    api_key: cloudinary_api_key,
-    api_secret: cloudinary_api_secret,
+    cloud_name: require('./vars').cloudinary_name,
+    api_key: require('./vars').cloudinary_api_key,
+    api_secret: require('./vars').cloudinary_api_secret,
   });
 };
