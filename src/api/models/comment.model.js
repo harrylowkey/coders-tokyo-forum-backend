440f905466071e5a633a9979ba22aa66282302c6
Quang Dang <quang.dang@homa.company>
2020-04-08 22:47:35 +0700

    comment models

---
+++ src/api/models/comment.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema(
+  {
+    postId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Post',
+    },
+    parentId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+postSchema.index(
+  { postId: 1 },
+  { parentId: 1 },
+  { createdAt: -1 },
+);
+
+const commentModel = mongoose.model('Comment', commentSchema);
+
+module.exports = {
+  schema: commentSchema,
+  model: commentModel,
+};
