a74ec7b3439cbdf7a35b88eae28b52d4c2256fbf
Quang Dang <quang.dang@homa.company>
2020-05-23 13:15:19 +0700

    comment arena GUI config

--- app.js
+++ app.js
@@ -14,15 +14,15 @@ const { port, redisConfig, arenaConfig } = require('./src/config/vars');
 const apiRoutes = require('./src/api/routes/index');
 const error = require('./src/middlewares/error-handler');
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('./swagger.json');
 const Arena = require('bull-arena');
 const QUEUES = require('./src/config/queues');
 const socket = require('@socket')
 
 app.use(cors())
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true }));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true }));
 
 mongoDB.connect();
 cloudinary.config();
@@ -32,22 +32,22 @@ app.use(cookieParser());
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-const arenaRoutes = Arena(
-  {
-    queues: QUEUES.map(q => ({
-      name: q.name,
-      prefix: q.options.prefix,
-      hostId: q.name,
-      redis: redisConfig,
-      type: 'bull',
-    })),
-  },
-  arenaConfig,
-);
+// const arenaRoutes = Arena(
+//   {
+//     queues: QUEUES.map(q => ({
+//       name: q.name,
+//       prefix: q.options.prefix,
+//       hostId: q.name,
+//       redis: redisConfig,
+//       type: 'bull',
+//     })),
+//   },
+//   arenaConfig,
+// );
 
 
 
-app.use('/arena', arenaRoutes);
+// app.use('/arena', arenaRoutes);
 app.use('/api/v1', apiRoutes);
 app.use(error.handler);
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
